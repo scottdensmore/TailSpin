d20d444c4b17541748244d034acfb2a7bd69fba2
scottdensmore <scottdensmore@mac.com>
2011-04-19 18:12:42 -0700

    Add jquery package and update logon

--- TailSpin/TailSpin.Web/AppRoutes.cs
+++ TailSpin/TailSpin.Web/AppRoutes.cs
@@ -46,6 +46,12 @@ namespace TailSpin.Web
                 "UploadLogo",
                 "{tenant}/MyAccount/UploadLogo",
                 new { controller = "Account", action = "UploadLogo" });
+
+            routes.MapRoute(
+               "Default", // Route name
+               "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+           );
         }
     }
 }
